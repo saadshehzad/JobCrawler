a049684747bb5a3fce063113df4538b841ac57f0
Muhammad Shehzad <xaadxhehzad@gmail.com>
2024-11-21 19:42:55 -0500

    Add functions to act as human

--- automation/selenium_crawler/human.py
+++ automation/selenium_crawler/human.py
@@ -1,15 +1,32 @@
+import time
+import random
+import pyautogui
 from abc import ABC, abstractmethod
 
 
 class Human(ABC):
+    
     def __init__(self):
         pass
 
-    def random_sleep(self):
+    def random_sleep_between_applying(self, min_sleep=3, max_sleep=10):
         """Introduce random delays."""
+        time.sleep(random.uniform(min_sleep, max_sleep))
 
-    def random_mouse_move(self):
+    def random_sleep_after_apply(self, min_sleep=12, max_sleep=20):
+        """Introduce random delays."""
+        time.sleep(random.uniform(min_sleep, max_sleep))
+
+    def random_sleep_after_applying_10_jobs(self, min_sleep=20, max_sleep=30):
+        """Introduce random delays."""
+        time.sleep(random.uniform(min_sleep, max_sleep))
+
+    def random_mouse_move(self, x_range=(0, 1920), y_range=(0, 1080)):
         """Simulate random mouse movements."""
+        x = random.randint(*x_range)
+        y = random.randint(*y_range)
+        duration = random.uniform(0.5, 3)
+        pyautogui.move(x, y, duration=duration)
 
     def random_scroll(self):
         """Simulate random scrolling behavior."""
