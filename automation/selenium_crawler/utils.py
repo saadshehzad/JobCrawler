02b3150815ad250c527a2231f369719edbaede48
Muhammad Shehzad <xaadxhehzad@gmail.com>
2024-12-08 21:51:32 -0500

    Add cookies methods to avoid login everytime

---
+++ automation/selenium_crawler/utils.py
@@ -0,0 +1,22 @@
+import pickle
+import os
+
+def save_cookies(driver, path="cookies.pkl"):
+    with open(path, "wb") as file:
+        pickle.dump(driver.get_cookies(), file)
+
+
+def load_cookies(driver, path="cookies.pkl"):
+    if not os.path.exists(path):
+        print("Cookie file not found. Proceeding with manual login.")
+        return
+
+    try:
+        with open(path, "rb") as file:
+            cookies = pickle.load(file)
+            for cookie in cookies:
+                driver.add_cookie(cookie)
+        
+        print("Cookies loaded successfully.")
+    except FileNotFoundError:
+        print("Cookie file not found. Proceeding with manual login.")
