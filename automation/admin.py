d183220cbcb321ae815feeb5b14cabc8e38ad53f
Muhammad Shehzad <xaadxhehzad@gmail.com>
2024-12-13 04:02:05 -0500

    Create model admin

--- automation/admin.py
+++ automation/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Website
+from .models import Website, SkillSet
 
 
 class WebsiteAdmin(admin.ModelAdmin):
@@ -9,4 +9,11 @@ class WebsiteAdmin(admin.ModelAdmin):
     list_filter = ("created_at", "updated_at")
 
 
+class SkillSetAdmin(admin.ModelAdmin):
+    list_display = ("skill_name", "country_name", "created_at", "updated_at")
+    search_fields = ("skill_name", "country_name")
+    list_filter = ("created_at", "updated_at")
+
+
 admin.site.register(Website, WebsiteAdmin)
+admin.site.register(SkillSet, SkillSetAdmin)
